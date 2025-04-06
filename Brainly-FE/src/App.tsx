a42af2c90a302b3d86948ee3c3c7cafdfe36d3df
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-04-06 20:53:46 +0500

    Cards done

--- Brainly-FE/src/App.tsx
+++ Brainly-FE/src/App.tsx
@@ -1,4 +1,4 @@
-import { Button } from './components';
+import { Button, Card } from './components';
 import { PlusIcon, ShareIcon } from './assets/icons';
 
 function App() {
@@ -31,6 +31,31 @@ function App() {
         onClick={() => {
           console.log("I clicked the button")
         }}
+      />
+      <Card
+        title='Twitter'
+        link='https://twitter.com/elonmusk/status/1686040632306819073'
+        type='twitter'
+      />
+      <Card
+        title='Youtube'
+        link='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        type='youtube'
+      />
+      <Card
+        title='Facebook'
+        link='https://www.facebook.com/NASA/posts/10161935695201772'
+        type='facebook'
+      />
+      <Card
+        title='Instagram'
+        link='https://www.instagram.com/p/CuNvGksOEcP/'
+        type='instagram'
+      />
+      <Card
+        title='LinkedIn'
+        link='urn:li:share:7083144825218863105'
+        type='linkedin'
       />
     </div>
   )
--- Brainly-FE/src/components/Card.tsx
+++ Brainly-FE/src/components/Card.tsx
@@ -1,9 +1,83 @@
-import React from 'react'
+import { ShareIcon } from "../assets/icons";
 
-function Card() {
-    return (
-        <div>Card</div>
-    )
+interface CardProps {
+    title: string;
+    link: string;
+    type: "twitter" | "youtube" | "linkedin" | "facebook" | "instagram";
 }
 
-export default Card
+function Card({ title, link, type }: CardProps) {
+    return (
+        <div className="bg-white shadow-md border border-gray-200 rounded-lg p-4 max-w-md mx-auto">
+
+            <div className="flex justify-between items-center mb-4 min-h-20">
+                <div className="flex items-center text-lg font-semibold text-gray-800">
+                    <ShareIcon />
+                    <div className="text-start ml-5"> {title}</div>
+                </div>
+            </div>
+
+            <div className="rounded-lg overflow-hidden bg-gray-100">
+                {type === "twitter" && (
+                    <blockquote className="twitter-tweet px-4 py-2">
+                        <p lang="zxx" dir="ltr">
+                            <a href={link} target="_blank" rel="noopener noreferrer">
+                                {link}
+                            </a>
+                        </p>
+                    </blockquote>
+                )}
+
+                {type === "youtube" && (
+                    <iframe
+                        className="w-full h-56"
+                        src={link.replace("watch?v=", "embed/")}
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                    ></iframe>
+                )}
+
+                {type === "facebook" && (
+                    <iframe
+                        className="w-full h-56"
+                        src={`https://www.facebook.com/plugins/post.php?href=${encodeURIComponent(
+                            link
+                        )}&width=500`}
+                        style={{ border: "none", overflow: "hidden" }}
+                        scrolling="no"
+                        frameBorder="0"
+                        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                        allowFullScreen
+                    ></iframe>
+                )}
+
+                {type === "instagram" && (
+                    <iframe
+                        className="w-full h-56"
+                        src={`${link}/embed`}
+                        frameBorder="0"
+                        allowTransparency={true}
+                        allow="encrypted-media"
+                        allowFullScreen
+                    ></iframe>
+                )}
+
+                {type === "linkedin" && (
+                    <iframe
+                        className="w-full h-56"
+                        src={`https://www.linkedin.com/embed/feed/update/${encodeURIComponent(
+                            link
+                        )}`}
+                        frameBorder="0"
+                        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                        allowFullScreen
+                    ></iframe>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Card;
