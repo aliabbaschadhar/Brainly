d18149fbe62aad5e8e9c636a32aff7dc4aa4c74a
Ali Abbas Chadhar <work.aliabbaschadhar@gmail.com>
2025-04-06 18:53:26 +0500

    Component struture almost done

---
+++ Brainly-FE/src/components/index.ts
@@ -0,0 +1,12 @@
+import Button from "./Button";
+import Card from "./Card";
+import Sidebar from "./Sidebar";
+import SidebarItem from "./SidebarItem";
+
+
+export {
+    Button,
+    Card,
+    Sidebar,
+    SidebarItem,
+}
